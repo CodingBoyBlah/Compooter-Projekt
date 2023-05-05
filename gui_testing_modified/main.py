a9c674da549908e7d94c5265af3d5e4b100e4e07
linette <127681427+linettebluemax@users.noreply.github.com>
2023-05-05 11:58:51 +0530

    .

--- gui_testing_modified/main.py
+++ gui_testing_modified/main.py
@@ -7,26 +7,70 @@ import mysql.connector as mys
 mycon = mys.connect(host = 'localhost', user = 'root', password = 'slay', database = 'torquecart')
 mycur = mycon.cursor()
 myemail="xyz"
+global arg=10 
+def AddToCart(slno):
+    x=10
+    return x
 
 
-class MainUI(QMainWindow)
- def __init__(self):
+
+
+class MainUI(QMainWindow):
+    def __init__(self):
         super(MainUI, self).__init__()
 
         # Load the ui file
-        loadUi('C:\\Users\\linet\\OneDrive\\Documents\\GitHub\\Compooter-Projekt\\gui_testing_modified\\cart.ui',self)
+        loadUi('C:\\Users\\linet\\OneDrive\\Documents\\GitHub\\Compooter-Projekt\\gui_testing_modified\\main_window.ui',self)
+
+        self.viewcart.clicked.connect(self.CartWindow)
+        self.tyres.clicked.connect(self.TyresWindow)
 
 
-class Cart(QDialog):
+
+    def TyresWindow(self):
+         dialog = Ui_TyresWindowDialog()
+         dialog.exec()
+
+    def CartWindow(self):
+        dialog = Ui_CartWindowDialog()
+        dialog.exec()
+
+class Ui_TyresWindowDialog(QDialog):
     def __init__(self):
-        super(MainUI, self).__init__()
+        super(Ui_TyresWindowDialog, self).__init__()
+
+        # Load the ui file
+        loadUi('C:\\Users\\linet\\OneDrive\\Documents\\GitHub\\Compooter-Projekt\\gui_testing_modified\\tyres.ui',self)
+
+        self.track.clicked.connect(self.TrackTyresWindow)
+
+    def TrackTyresWindow(self):
+        dialog = Ui_TrackTyresWindowDialog()
+        dialog.exec()
+
+class Ui_TrackTyresWindowDialog(QDialog):
+    def __init__(self):
+        super(Ui_TrackTyresWindowDialog, self).__init__()
+
+        # Load the ui file
+        loadUi('C:\\Users\\linet\\OneDrive\\Documents\\GitHub\\Compooter-Projekt\\gui_testing_modified\\tracktyrestest.ui',self)
+
+        self.add_10.clicked.connect(AddToCart(arg))
+        self.add_11.clicked.connect(AddToCart(arg))
+        self.add_12.clicked.connect(AddToCart(arg))
+
+
+
+class Ui_CartWindowDialog(QDialog):
+    def __init__(self):
+        super(Ui_CartWindowDialog, self).__init__()
 
         # Load the ui file
         loadUi('C:\\Users\\linet\\OneDrive\\Documents\\GitHub\\Compooter-Projekt\\gui_testing_modified\\cart.ui',self)
 
   
         # When button pressed, Open new window
-        self.ADD.clicked.connect(self.CartWindow)
+        #self.ADD.clicked.connect(self.CartWindow)
         self.BUY.clicked.connect(self.PaymentWindow)
         #self.show()
         self.ProductList.itemChanged.connect(self.updateAmt)
@@ -51,9 +95,7 @@ class Cart(QDialog):
             self.ProductList.setItem(i,4,QTableWidgetItem(str(amount)))
             i=i+1
 
-    def CartWindow(self):
-        dialog = Ui_CartWindowDialog()
-        dialog.exec()
+
 
 
     def PaymentWindow(self):
@@ -66,24 +108,26 @@ class Cart(QDialog):
           prevCol = int (item.column()) - 1;
           nextCol = int (item.column()) - 1;
           updatedQty = self.ProductList.item(item.row(), item.column()).text()
+
           ItemType = self.ProductList.item(item.row(), prevCol).text()
           amount = self.ProductList.item(item.row(), nextCol).text()
 
-          q2="SELECT remqty, price from product WHERE item  = %s"
+          q2="SELECT remqty, price, slno from product WHERE item  = %s"
           mycur.execute(q2,(ItemType,))
           result = mycur.fetchall()
 
           #check sufficient qty
           Qty = int(result[0][0])
-          if (int(updatedQty) > Qty ):
+
+          if ("" != updatedQty and int(updatedQty) > Qty ):
+            self.ProductList.setItem(item.row(), item.column(),QTableWidgetItem("1"))
             msgBox = QMessageBox()
             msgBox.setText("Quantity selected not available")
             msgBox.exec()
-            temp=1
-            self.ProductList.setItem(0,3,QTableWidgetItem(temp))
           else:
-            amount = int(result[0][1]) * int(updatedQty) 
-            self.ProductList.setItem(item.row(),4,QTableWidgetItem(str(amount))) 
+            if ("" != updatedQty):
+                amount = int(result[0][1]) * int(updatedQty) 
+                self.ProductList.setItem(item.row(),4,QTableWidgetItem(str(amount))) 
 
           
           #self.ProductList.setItem(i,4,QTableWidgetItem(str(amount)))
