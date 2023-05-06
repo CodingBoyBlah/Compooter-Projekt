4ede9894e2d8be47864c7d4af07640d235a77ab9
linette <127681427+linettebluemax@users.noreply.github.com>
2023-05-06 20:24:09 +0530

    .

--- gui_testing_modified/tyre.py
+++ gui_testing_modified/tyre.py
@@ -5,14 +5,40 @@
 
 '''
 
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QLineEdit, QDialog
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QLineEdit, QDialog, QMessageBox
 from PyQt5 import QtWidgets
 from PyQt5 import uic
 from pathlib import Path
 
+import mysql.connector as mys
+mycon = mys.connect(host = 'localhost', user = 'root', password = 'slay', database = 'torquecart')
+mycur = mycon.cursor()
+myemail="xyz"
+
+
+ #Add item to Cart
+def AddToCart(slno):        
+    
+    q4="SELECT price , item from products WHERE slno  = %s"
+    mycur.execute(q4,(slno,))
+    result = mycur.fetchall()
+
+    global myemail
+
+    q3 = "insert into cart (slno,qty,amt,email) values ('{0}','{1}','{2}','{3}')".format(slno,1, result[0][0],myemail)
+    
+    mycur.execute(q3)
+    mycon.commit()
+
+    msgBox = QMessageBox()
+    mesg = "Added "+result[0][1]+" to cart"
+    msgBox.setText(mesg)
+    msgBox.exec()
+
+
 ###############################################################################
 class Ui_commonTyresWindow(QMainWindow):
-    def __init__(self, ui_file):
+    def __init__(self, ui_file, subType):
         super(Ui_commonTyresWindow, self).__init__()
 
         self.ui_name = ui_file[:-3]
@@ -21,34 +47,41 @@ class Ui_commonTyresWindow(QMainWindow):
         # Load the ui file
         uic.loadUi(filepath,self)
 
-        self.button1 = self.findChild(QPushButton, 'SignIN')
-        self.button2 = self.findChild(QPushButton, 'SignIN_2')
-        self.button3 = self.findChild(QPushButton, 'SignIN_3')
 
-        self.button1.clicked.connect(self.button1_handler)
-        self.button2.clicked.connect(self.button2_handler)
-        self.button3.clicked.connect(self.button3_handler)
+        if (subType == "track"):
+            self.button1 = self.findChild(QPushButton, 'add_10')
+            self.button2 = self.findChild(QPushButton, 'add_11')
+            self.button3 = self.findChild(QPushButton, 'add_12')
+
+            self.button1.clicked.connect(lambda:AddToCart(10))
+            self.button2.clicked.connect(lambda:AddToCart(11))
+            self.button3.clicked.connect(lambda:AddToCart(12))
 
 
+        if (subType == "offroad"):
+            self.button1 = self.findChild(QPushButton, 'add_13')
+            self.button2 = self.findChild(QPushButton, 'add_14')
+            self.button3 = self.findChild(QPushButton, 'add_15')
 
-    def button1_handler(self):
-        print(f"{self.ui_name} button1_handler")
-        pass
+            self.button1.clicked.connect(lambda:AddToCart(13))
+            self.button2.clicked.connect(lambda:AddToCart(14))
+            self.button3.clicked.connect(lambda:AddToCart(15))
 
-    def button2_handler(self):
-        print(f"{self.ui_name} button2_handler")
-        pass
 
-    def button3_handler(self):
-        print(f"{self.ui_name} button3_handler")
-        pass
+        if (subType == "street"):
+            self.button1 = self.findChild(QPushButton, 'add_16')
+            self.button2 = self.findChild(QPushButton, 'add_17')
+            self.button3 = self.findChild(QPushButton, 'add_18')
 
+            self.button1.clicked.connect(lambda:AddToCart(16))
+            self.button2.clicked.connect(lambda:AddToCart(17))
+            self.button3.clicked.connect(lambda:AddToCart(18))
 
 
 ###############################################################################
 
 class Ui_tyreWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self,loginUser):
         super(Ui_tyreWindow, self).__init__()
 
         filepath = Path(__file__).parent.resolve()
@@ -64,25 +97,29 @@ class Ui_tyreWindow(QMainWindow):
         self.offroad.clicked.connect(self.offroad_handler)
         self.street.clicked.connect(self.street_handler)
 
-
+        global myemail
+        myemail = loginUser
 
     def track_handler(self):
         print("track_handler")
-        self.trackTyres = Ui_commonTyresWindow('track_tyres.ui')
+        self.trackTyres = Ui_commonTyresWindow('track_tyres.ui','track')
         self.trackTyres.show()
         pass
 
     def offroad_handler(self):
         print("offroad_handler")
-        self.offroadTyres = Ui_commonTyresWindow('offroad tyres.ui') #Ui_offroadTyresWindow()
+        self.offroadTyres = Ui_commonTyresWindow('offroad tyres.ui','offroad') #Ui_offroadTyresWindow()
         self.offroadTyres.show()
         pass
 
     def street_handler(self):
         print("street_handler")
-        self.offroad_suspension = Ui_commonTyresWindow('street tyres.ui') #Ui_offroadSuspensionWindow()
-        self.offroad_suspension.show()
+        self.streetTyres = Ui_commonTyresWindow('street tyres.ui','street')#Ui_offroadSuspensionWindow()
+        self.streetTyres.show()
         pass
+
+
+
 
 
 if __name__ == "__main__":
